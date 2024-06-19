734238dbd50ce4063df86bf424272ca5746b7cd9
Swarna1806 <143710685+Swarna1806@users.noreply.github.com>
2024-06-19 12:47:19 +0530

    Update userController.js
    2nd commit for userController

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,55 +1,61 @@
-const User = require('../Models/userModel')
-const bcrypt = require('bcryptjs')
-const crypto = require('crypto')
+const User = require('../Models/userModel');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 exports.signUp = async (req, res) => {
   try {
-    req.body.role = "user"
-    const newUser = await User.create(req.body)
+    req.body.role = "user";
+    const newUser = await User.create(req.body);
     res.status(201).json({
       message: "Success",
       body: newUser
-    })
+    });
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
     res.status(404).json({
       message: "Error in SignUp of user",
       body: err
-    })
+    });
   }
-}
+};
 
 exports.login = async (req, res) => {
   try {
-    const email = req.body.email
-    const pass = req.body.password
+    const email = req.body.email;
+    const pass = req.body.password;
     if (!email || !pass) {
       return res.status(400).json({
-        message: "Please send username and password to the api"
-      })
+        message: "Please send username and password to the API"
+      });
     }
 
-    const user = await User.findOne({ email })
-    const isValidPass = await bcrypt.compare(pass, user.password)
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Incorrect Email or password"
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(pass, user.password);
 
     if (isValidPass) {
       res.status(200).json({
         message: "Login Success",
         role: user.role,
         userId: user._id
-      })
+      });
     } else {
       return res.status(401).json({
         message: "Incorrect Email or password"
-      })
+      });
     }
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
     res.status(404).json({
       message: err.message
-    })
+    });
   }
-}
+};
 
 exports.protect = (req, res, next) => {
   try {
@@ -57,26 +63,26 @@ exports.protect = (req, res, next) => {
       req.user = req.session.user;
     }
     if (!req.user) {
-      return res.status(404).json({
+      return res.status(401).json({
         message: "You are not logged in"
       });
     }
   } catch (err) {
-    console.log(`okok :${err}`);
+    console.log(`Error: ${err}`);
     return res.status(404).json({
       message: err
     });
   }
   next();
-}
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     try {
       if (!roles.includes(req.user.role)) {
-        console.log("no permission to access this route");
-        return res.status(404).json({
-          message: "You Dont have permission to access this route"
+        console.log("No permission to access this route");
+        return res.status(403).json({
+          message: "You don't have permission to access this route"
         });
       }
     } catch (err) {
@@ -87,4 +93,4 @@ exports.restrictTo = (...roles) => {
     }
     next();
   };
-}
+};
